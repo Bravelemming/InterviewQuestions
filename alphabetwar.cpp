dfcec3c5579ee589ac523786f472507b8a818ac0
bravelemming <bravelemming@gmail.com>
2018-11-06 14:52:45 -0800

    fixed an error in the case switch, default now breaks.

--- alphabetwar.cpp
+++ alphabetwar.cpp
@@ -1,3 +1,19 @@
+/*
+Task
+Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+The left side letters and their power:
+w - 4
+p - 3
+b - 2
+s - 1
+The right side letters and their power:
+m - 4
+q - 3
+d - 2
+z - 1
+*/
+
 #include <string>
 using namespace std;
 
@@ -29,7 +45,7 @@ string alphabetWar(string fight)
     case 'z': right += 1;
         break;
         
-    default: return "wrong string!";
+    default: break;
     }
   }
 
@@ -40,22 +56,3 @@ string alphabetWar(string fight)
   else {return "Let's fight again!";}
   
 }
-
-/*
-Task
-Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
-
-The left side letters and their power:
-
-w - 4
-p - 3
-b - 2
-s - 1
-The right side letters and their power:
-
-m - 4
-q - 3
-d - 2
-z - 1
-
-*/
