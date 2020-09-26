8c7235e66afa53b1f7e7bd0bcdd1f6ac37b0165e
Jack Daniel Kinne <bravelemming@gmail.com>
2020-09-26 11:02:34 -0700

    Create MaximumDepthOfBinaryTree.java
    Runtime: 0 ms, faster than 100.00% of Java online submissions for Maximum Depth of Binary Tree.
    Memory Usage: 38.3 MB, less than 100.00% of Java online submissions for Maximum Depth of Binary Tree.

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,27 @@
+/**
+ * Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ * Note: A leaf is a node with no children.
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        //base case
+        if (root == null) return 0; //might need to be 0
+        //compare heights, keep larger
+        //return that depth as int
+        return Math.max(maxDepth(root.left)+1,maxDepth(root.right)+1);
+    }
+}
